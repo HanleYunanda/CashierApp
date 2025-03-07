7b518148afcac0edceb36923ea45f9a3a7dbce95
Hanley Yunanda <hanleyunanda25@gmail.com>
2025-03-07 13:35:28 +0700

    add middleware logger & errorHandler

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const cors = require('cors')
 
 const connectDb = require('./config/dbConfig')
 const corsOptions = require('./config/corsOptions')
+const { logger, logEvents } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
 
 const PORT = process.env.PORT || 3500
 connectDb()
 
 // Middleware
+app.use(logger)
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
@@ -27,6 +30,8 @@ app.all('*', (req, res) => {
     res.status(404).json({message: "404 Not Found"})
 })
 
+app.use(errorHandler)
+
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB')
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
@@ -34,5 +39,5 @@ mongoose.connection.once('open', () => {
 
 mongoose.connection.on('error', err => {
     console.log(err)
-    // logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
 })
